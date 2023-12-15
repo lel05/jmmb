a3a6290f32fc2c1a32c51fe3e4456ca6d036a098
4PTP_1 <4PTP_1@DESKTOP-HMD73UL>
2023-12-15 11:06:59 +0100

    Dodana funkcjonalność minutnika, zaktualizowany widok minutnika.

--- jmmb/MainPage.xaml.cs
+++ jmmb/MainPage.xaml.cs
@@ -8,8 +8,10 @@ using Xamarin.Forms;
 
 namespace jmmb
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
+        bool isTimeRunning = false;
+        TimeSpan remainingTime = TimeSpan.Zero;
         public MainPage()
         {
             InitializeComponent();
@@ -17,7 +19,28 @@ namespace jmmb
 
         private void startButton_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                int minutes = int.Parse(entryTime.Text);
+                remainingTime = TimeSpan.FromMinutes(minutes);
+                isTimeRunning = true;
+                Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+                {
+                    remainingTimeLabel.Text = remainingTime.ToString("mm\\:ss");
+                    remainingTime = remainingTime.Subtract(TimeSpan.FromSeconds(1));
+                    if (remainingTime.TotalSeconds <= 0)
+                    {
+                        remainingTimeLabel.Text = remainingTime.ToString("mm\\:ss");
+                        isTimeRunning = false;
+                        DisplayAlert("Uwaga!", "Czas minął.", "OK");
+                    }
+                    return isTimeRunning;
+                });
+        }
+            catch (Exception ex)
+            {
+                DisplayAlert("Błąd!", "Wprowadzono niepoprawne dane.", "OK");
+            }
         }
     }
 }
