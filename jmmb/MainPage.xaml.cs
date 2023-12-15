84a6d2ed9494d81e9314e5d58d1cd705c1a99559
lel05 <graczkuba2005@gmail.com>
2023-12-15 10:29:40 +0100

    update

--- jmmb/MainPage.xaml.cs
+++ jmmb/MainPage.xaml.cs
@@ -16,7 +16,8 @@ namespace jmmb
 
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
-                clockLabel.Text = DateTime.Now.ToString();
+                clockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+                clockDateLabel.Text = DateTime.Today.ToString("dd MMMM yyy");
                 return true;
             });
 
